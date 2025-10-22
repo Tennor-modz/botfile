53b90010a5f367eed560a63161284836445d3fae
Tennor-modz <giddysarry@gmail.com>
2025-10-22 15:44:51 +0300

    Update config.js

--- config.js
+++ config.js
@@ -1,30 +1,11 @@
+require('dotenv').config();
+
 module.exports = {
-  BOT_NAME: process.env.BOT_NAME || 'Trashcore',
-  
-  OWNER_NAME: process.env.OWNER_NAME || 'Trashcore',
-  
-  OWNER_NUMBER: process.env.OWNER_NUMBER || '255778104517',
-  
-  SESSION_DIR: process.env.SESSION_DIR || './trash_baileys',
-  
-  SESSION_ID: process.env.SESSION_ID || '', // 🧠 your session ID or base64 session
-  NO_PREFIX: process.env.NO_PREFIX ? process.env.NO_PREFIX === 'true' : true,
-  
-  STATUS_VIEW: process.env.STATUS_VIEW ? process.env.STATUS_VIEW === 'true' : true
+  BOT_NAME: process.env.BOT_NAME,
+  OWNER_NAME: process.env.OWNER_NAME,
+  OWNER_NUMBER: process.env.OWNER_NUMBER,
+  SESSION_DIR: process.env.SESSION_DIR,
+  SESSION_ID: process.env.SESSION_ID,
+  NO_PREFIX: process.env.NO_PREFIX === 'true',
+  STATUS_VIEW: process.env.STATUS_VIEW === 'true',
 };
-
-
-
-
-
-/*
-> Recode script give credits to›
-Giddy Tennor(Trashcore)
-
-📝 | Created By Trashcore
-🖥️ | Base Ori By Trashcore 
-📌 |Credits Putrazy Xd
-📱 |Chat wa:254104245659
-👑 |Github: Tennor-modz 
-✉️ |Email: giddytennor@gmail.com
-*/
